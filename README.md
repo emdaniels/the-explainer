86b210001aff0f1f16e378491da414f735d6c712
Emily Daniels <emily@emilydaniels.com>
2016-11-30 21:13:44 -0500

    initial project commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# the-explainer
-Augments text with the definitions of words not found in the Basic English word list.
+# The Explainer
+
+Augments
+(enlarge or increase)
+
+text with the definitions 
+(a concise explanation of the meaning of a word or phrase or symbol)
+
+of words 
+(the words that are spoken)
+
+not found in the [Basic English](https://en.wiktionary.org/wiki/Appendix:1000_basic_English_words) word list. 
+
+
+References
+==========
+
+Heart of Darkness
+-----------------
+
+The Project Gutenberg eBook of Heart of Darkness, by Joseph Conrad:
+
+http://www.gutenberg.org/ebooks/526
+
+This eBook is for the use of anyone anywhere at no cost and with
+almost no restrictions whatsoever.  You may copy it, give it away or
+re-use it under the terms of the Project Gutenberg License included
+with this eBook or online at www.gutenberg.net
+
+NLTK
+----
+
+NLTK -- the Natural Language Toolkit -- is a suite of open source
+Python modules, data sets and tutorials supporting research and
+development in Natural Language Processing.
+
+https://github.com/nltk/nltk/blob/develop/LICENSE.txt
+
+
+License
+=======
+
+Copyright 2016 Emily Daniels
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ explain.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python
+"""
+Filename: explain.py
+Author: Emily Daniels
+Date: November 2016
+Purpose: Augments text with the definitions of words not found
+in the Basic English word list.
+"""
+
+import nltk
+from nltk.corpus import wordnet
+
+pos_to_define = ['JJ', 'JJS', 'JJR', 'NN', 'NNS', 'RB', 'RBR', 'RBS', 'VB',
+                 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+
+
+def tag_text(text):
+    token_wordy_text = nltk.word_tokenize(text)
+    return nltk.pos_tag(token_wordy_text)
+
+
+def define_word(word, pos):
+    top_def = ''
+    synsets = wordnet.synsets(word, pos)
+    if len(synsets) > 0:
+        defined = synsets[0].definition()
+        top_def = '\n(' + defined + ')\n'
+    return top_def
+
+
+def define_by_pos(tagged_wordy_text):
+    basic_text = []
+    definition = ''
+    for word, pos in tagged_wordy_text[:]:
+        for pos_def in pos_to_define:
+            if pos == pos_def:
+                if any(word in s for s in basic_english):
+                    continue
+                else:
+                    if pos == 'JJ' or pos == 'JJS' or pos == 'JJR':
+                        definition = define_word(word, 'a')
+                    elif pos == 'NN' or pos == 'NNS':
+                        definition = define_word(word, 'n')
+                    elif pos == 'RB' or pos == 'RBR' or pos == 'RBS':
+                        definition = define_word(word, 'r')
+                    elif pos == 'VB' or pos == 'VBD' or pos == 'VBG' \
+                            or pos == 'VBN' or pos == 'VBP' or pos == 'VBZ':
+                        definition = define_word(word, 'v')
+        basic_text.append((word + ' ' + definition).encode('utf-8'))
+        definition = ''
+    return basic_text
+
+
+def chunk_text(text, length):
+    all_sent = []
+    temp = ""
+    for word in text:
+        new_sent = word
+        temp += new_sent
+        if len(temp) >= length:
+            all_sent.append(temp)
+            temp = ""
+    return all_sent
+
+
+if __name__ == "__main__":
+    with open("basicEnglish.txt", "rU") as f:
+        basic_english = f.read().split('\n')
+
+    with open("intro.txt", "rU") as f:
+        wordy_text = f.read().decode('latin-1').encode('utf-8').decode('utf-8')
+
+    basic_text = define_by_pos(tag_text(wordy_text))
+    chunked_text = chunk_text(basic_text, 80)
+
+    with open("intro_Explained.txt", "wb") as f:
+        for sent in chunked_text:
+            f.write(sent + "\n")
